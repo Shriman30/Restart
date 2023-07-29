3c21b705411551594b93afdcdd6ccf92a50906e6
Shriman <shriman30@hotmail.com>
2023-07-28 22:08:39 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Admin on 2023-07-28.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
